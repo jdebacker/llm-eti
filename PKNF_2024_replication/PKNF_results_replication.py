c3ab2ff55ec9cecb8f48d1e6dfb72eaa0b56561a
jdebacker <jason.debacker@gmail.com>
2024-11-06 20:40:37 -0500

    tex tables, ETI est

--- PKNF_2024_replication/PKNF_results_replication.py
+++ PKNF_2024_replication/PKNF_results_replication.py
@@ -15,7 +15,7 @@ px.defaults.template = "plotly_white"
 
 # %%
 # Read in data
-DATA_FILENAME = "PKNF_replication_results"
+DATA_FILENAME = "PKNF_replication_results_gpt-4o-mini"
 df = pickle.load(
     open(os.path.join(CUR_DIR, "..", "data", DATA_FILENAME + ".pkl"), "rb")
 )
@@ -28,10 +28,10 @@ df = df.dropna(subset=["income"])
 
 # %%
 # Create a pre-post variable
-df["Post"] = df["round_num"] >= 8
+df["Post"] = (df["round_num"] >= 8).astype(int)
 # create labor supply variable
 df["labor"] = (df["income"] / 20).astype(int)
-df["labor_20"] = df["labor"] <= 20
+df["labor_20"] = (df["labor"] <= 20).astype(int)
 df["lab_supply"] = df["labor"] / df["max_labor"]
 
 # %%
@@ -53,12 +53,14 @@ treat_dict = {
     2: "Prog,Flat25",
     3: "Prog,Flat50",
     4: "Flat25,Prog",
+    5: "Flat50,Prog",
 }
 table5["Treatment"] = table5["Treatment"].map(treat_dict)
 # Format decimals
 
 # Save as markdown
 table5.to_markdown(os.path.join(CUR_DIR, "tables_figures", "table5.md"), floatfmt=".2f", index=False)
+table5.to_latex(os.path.join(CUR_DIR, "tables_figures", "table5.tex"), float_format="%.2f", index=False)
 # TODO: add p-value for test of diffs in means
 
 # %%
@@ -76,18 +78,18 @@ df_bar["Treatment"] = df_bar["treatment"].map(treat_dict)
 treat_num = 0
 for treat in df_bar["Treatment"].unique():
     treat_num += 1
-    fig = px.bar(
+    fig = px.histogram(
         df_bar[df_bar["Treatment"] == treat],
         x="max_labor",
         y="labor",
         color="Post",
         barmode="group",
-        title=f"Figure 2: Labor Supply by Treatment ({treat})",
+        # title=f"Figure 2: Labor Supply by Treatment ({treat})",
     )
     # Specify the x- and y-axis labels
     fig.update_xaxes(title_text="Maximum Labor")
     fig.update_yaxes(title_text="Labor Supply, in Units")
-    fig.write_image(os.path.join(CUR_DIR, "tables_figures", "LLM_Fig2_{treat_num}.png"))
+    fig.write_image(os.path.join(CUR_DIR, "tables_figures", f"LLM_Fig2_{treat_num}.png"))
 
 # %%
 # Figure 4
@@ -101,18 +103,18 @@ df_bar = (
 )
 # update treatment to be more descriptive
 df_bar["Treatment"] = df_bar["treatment"].map(treat_dict)
-fig = px.line(
+fig = px.scatter(
     df_bar,
     x="round_num",
     y="lab_supply",
     color="Treatment",
-    title="Figure 4: Mean Labor Supply by Period and Treatment",
-)
+    # title="Figure 4: Mean Labor Supply by Period and Treatment",
+).update_traces(mode='lines+markers')
 # Specify the x- and y-axis labels
 fig.update_xaxes(title_text="Period")
 fig.update_yaxes(title_text="Labor Supply in %")
 # put a vertical dashed line at period 8
-fig.add_vline(x=8, line_dash="dash")
+fig.add_vline(x=7.5, line_dash="dash")
 fig.show()
 fig.write_image(os.path.join(CUR_DIR, "tables_figures", "LLM_Fig4.png"))
 
@@ -120,32 +122,30 @@ fig.write_image(os.path.join(CUR_DIR, "tables_figures", "LLM_Fig4.png"))
 # Regression results
 
 PKNF_Table6_Col1 = [
-    -0.007, "(0.006)", -0.052, "(0.022)", 0.083, "(0.009)", 0.947, "(0.055)", 3344, 0.035
+    "-0.007", "(0.006)", "-0.052", "(0.022)", "0.083", "(0.009)", "0.947", "(0.055)", "3344", "0.035"
 ]
 
 # labor = constant  + post + treat + post*treat
 # Table 6
 # Create a post-treatment interaction variable
-df["treated"] = df["treatment"] > 1
+df["treated"] = (df["treatment"] > 1).astype(int)
 df["post_treat"] = df["Post"] * df["treated"]
 df["Treatment"] = df["treatment"].map(treat_dict)
 reg_results_dict = {}
-for treat in df["Treatment"].unique():
+for treat in ["Prog,Flat25", "Prog,Flat50", "Flat25,Prog", "Flat50,Prog"]:
     # Keep the treatment and control
-    df_reg = df[(df["Treatment"] == treat) | (df["treated"] == 1)]
+    df_reg = df[(df["Treatment"] == treat) | (df["treatment"] == 1)]
     # specify an indicator for the treated group
-    df_reg["treated_group"] = 0
-    df_reg.loc[df_reg["treatment"] > 1, "treated_group"] = 1
-    model = smf.ols('lab_supply ~ Post + treated_group + post_treat', data=df_reg)
+    model = smf.ols('lab_supply ~ Post + treated + post_treat', data=df_reg)
     # Estimate the model
     results = model.fit()
     reg_results_dict[treat] = [] # initialize list to store results
-    reg_results_dict[treat].append(f"{results.params["Post[T.True]"]:.3f}")
-    reg_results_dict[treat].append(f"({results.bse["Post[T.True]"]:.3f})")
-    reg_results_dict[treat].append(f"{results.params["treated_group"]:.3f}")
-    reg_results_dict[treat].append(f"({results.bse["treated_group"]:.3f})")
-    reg_results_dict[treat].append(f"{results.params["post_treat[T.True]"]:.3f}")
-    reg_results_dict[treat].append(f"({results.bse["post_treat[T.True]"]:.3f})")
+    reg_results_dict[treat].append(f"{results.params["Post"]:.3f}")
+    reg_results_dict[treat].append(f"({results.bse["Post"]:.3f})")
+    reg_results_dict[treat].append(f"{results.params["treated"]:.3f}")
+    reg_results_dict[treat].append(f"({results.bse["treated"]:.3f})")
+    reg_results_dict[treat].append(f"{results.params["post_treat"]:.3f}")
+    reg_results_dict[treat].append(f"({results.bse["post_treat"]:.3f})")
     reg_results_dict[treat].append(f"{results.params["Intercept"]:.3f}")
     reg_results_dict[treat].append(f"({results.bse["Intercept"]:.3f})")
     # for i, val in enumerate(results.params):
@@ -157,6 +157,7 @@ reg_results = pd.DataFrame(reg_results_dict)
 reg_results.index = ["Post", "", "Treated", "", "Post*Treated", "", "Constant", "", "N", "R-squared"]
 # Save as markdown
 reg_results.to_markdown(os.path.join(CUR_DIR, "tables_figures", "table6.md"), index=True)
+reg_results.to_latex(os.path.join(CUR_DIR, "tables_figures", "table6.tex"), index=True)
 
 # Make table comparing to PKNF results to LLM results
 compare_reg_dict = {
@@ -167,6 +168,8 @@ reg_results = pd.DataFrame(compare_reg_dict)
 reg_results.index = ["Post", "", "Treated", "", "Post*Treated", "", "Constant", "", "N", "R-squared"]
 # Save as markdown
 reg_results.to_markdown(os.path.join(CUR_DIR, "tables_figures", "table6_compare.md"), index=True)
+reg_results.to_latex(os.path.join(CUR_DIR, "tables_figures", "table6_compare.tex"), index=True)
+
 
 
 
